b603d1c8b26c840c0f164687197213217eda8b03
Nicu Parente <nicuparente@gmail.com>
2017-07-24 19:59:04 -0700

    initialized necessary files. Added category reducer

---
+++ lab-nicu/src/reducer/category.js
@@ -0,0 +1,20 @@
+'use strict'
+
+const initialState = []
+
+export default (state = initialState, action) => {
+  let { type, payload } = action
+
+  switch (type) {
+  case 'CATEGORY_CREATE':
+    return [...state, payload]
+
+  case 'CATEGORY_UPDATE':
+    return state.filter(category => category.id === payload.id ? payload : category)
+  
+  case 'CATEGORY_DELETE':
+    return state.filter(category => category.id !== payload.id)
+  default:
+    return initialState
+  }
+}
